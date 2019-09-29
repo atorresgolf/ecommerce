22450ed53c062bbf1fe4db46a37ecef5b9a68141
Anto <atorresgolf@gmail.com>
2019-09-29 19:51:39 -0300

    registroExitoso

--- registroExitoso.php
+++ registroExitoso.php
@@ -5,11 +5,12 @@
 	<title>Registro Exitoso</title>
 </head>
 <body>
-	<header>
+	<!--<header>
 		<a href="index.php" class="volver"><img src="img/shopgolf.png" alt=""></a>
 		<a href="index.php" class="volver1"><i class="fas fa-2x fa-home" style="color:#18EB8E"></i></a>
-	</header>
-	
-	<h2>¡Tu registro fue exitoso!</h2>
+	</header>-->
+	<?php include "header.php"; ?>
+	<h2 style= "text-align:center;">¡Tu registro fue exitoso!</h2>
+
 </body>
 </html>
